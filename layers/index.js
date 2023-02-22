5289171abf791a3773824138effd44aba0c23069
Allen Helton <allenheltondev@gmail.com>
2023-02-22 13:28:06 -0600

    Update to sdk v1

--- layers/index.js
+++ layers/index.js
@@ -1,62 +1,38 @@
-const { SimpleCacheClient, CacheGetStatus } = require('@gomomento/sdk');
+const { SimpleCacheClient, EnvMomentoTokenProvider, Configurations, CacheGet } = require('@gomomento/sdk');
 const { SecretsManagerClient, GetSecretValueCommand } = require('@aws-sdk/client-secrets-manager');
-const { StatusCodes } = require('http-status-codes');
 const hash = require('object-hash');
 
 const IDEMPOTENCY_CACHE = 'idempotency-cache';
 const PAYLOAD_MISMATCH_ERROR = 'The payload in the request does not match the original payload with the provided idempotency key.';
 const REQUEST_IN_PROGRESS_ERROR = 'The original request with the matching idempotency key is still being processed.';
 const secretsManager = new SecretsManagerClient();
+
 let momentoClient;
-let momentoAuthToken;
-
-exports.getCacheClient = async () => {
-  if (!momentoClient) {
-    const authToken = await exports.getCacheAuthToken();
-    const cacheClient = new SimpleCacheClient(authToken, Number(process.env.CACHE_TTL));
-    momentoClient = cacheClient;
-
-    try{
-      await momentoClient.createCache(IDEMPOTENCY_CACHE);
-    } catch(err){
-      console.info(err);
-    }
-  }
-  return momentoClient;
-};
-
-exports.getCacheAuthToken = async () => {
-  if (!momentoAuthToken) {
-    const command = new GetSecretValueCommand({ SecretId: process.env.CACHE_AUTH_TOKEN_SECRET });
-    const result = await secretsManager.send(command);
-    momentoAuthToken = JSON.parse(result.SecretString).auth_token;
-  }
-  return momentoAuthToken;
-};
+let cachedSecret;
 
 exports.validateCache = async (idempotencyKey, payload) => {
-  const cacheClient = await exports.getCacheClient();
-  const cacheResponse = await cacheClient.get(IDEMPOTENCY_CACHE, idempotencyKey);
-  if (cacheResponse.status == CacheGetStatus.Miss) {
+  const momento = await getCacheClient();
+  const cacheResponse = await momento.get(IDEMPOTENCY_CACHE, idempotencyKey);
+  if (cacheResponse instanceof CacheGet.Miss) {
     const cacheValue = {
       hash: hash(payload),
       inProgress: true
     };
 
-    await cacheClient.set(IDEMPOTENCY_CACHE, idempotencyKey, JSON.stringify(cacheValue));
-  } else if (cacheResponse.status == CacheGetStatus.Hit){
-    const cacheValue = JSON.parse(cacheResponse.text());
+    await momento.set(IDEMPOTENCY_CACHE, idempotencyKey, JSON.stringify(cacheValue));
+  } else if (cacheResponse instanceof CacheGet.Hit){
+    const cacheValue = JSON.parse(cacheResponse.valueString());
     const payloadHash = hash(payload);
     if(payloadHash !== cacheValue.hash){
       return {
-        statusCode: StatusCodes.BAD_REQUEST,
+        statusCode: 400,
         error: PAYLOAD_MISMATCH_ERROR
       };
     }
-    console.log(cacheValue);
+    
     if(cacheValue.inProgress){
       return {
-        statusCode: StatusCodes.ACCEPTED,
+        statusCode: 202,
         error: REQUEST_IN_PROGRESS_ERROR
       };
     }
@@ -69,13 +45,13 @@ exports.validateCache = async (idempotencyKey, payload) => {
 };
 
 exports.finalizeCache = async (idempotencyKey, statusCode, result) => {
-  const cacheClient = await exports.getCacheClient();
+  const momento = await getCacheClient();
   if(statusCode >= 400) {
     // If the request resulted in an error, do not save the response and allow the caller to attempt to fix it
-    await cacheClient.delete(IDEMPOTENCY_CACHE, idempotencyKey);
+    await momento.delete(IDEMPOTENCY_CACHE, idempotencyKey);
   } else {
-    const cacheResponse = await cacheClient.get(IDEMPOTENCY_CACHE, idempotencyKey);
-    const existingCacheValue = JSON.parse(cacheResponse.text());
+    const cacheResponse = await momento.get(IDEMPOTENCY_CACHE, idempotencyKey);
+    const existingCacheValue = JSON.parse(cacheResponse.valueString());
 
     const cacheResult = {
       hash: existingCacheValue.hash,
@@ -83,6 +59,44 @@ exports.finalizeCache = async (idempotencyKey, statusCode, result) => {
       statusCode
     };
 
-    await cacheClient.set(IDEMPOTENCY_CACHE, idempotencyKey, JSON.stringify(cacheResult));
+    await momento.set(IDEMPOTENCY_CACHE, idempotencyKey, JSON.stringify(cacheResult));
   }
 };
+
+const getCacheClient = async (caches = [IDEMPOTENCY_CACHE]) => {
+  if (!momentoClient) {
+    const authToken = await getCacheAuthToken();
+    process.env.AUTH_TOKEN = authToken;
+    const credentials = new EnvMomentoTokenProvider({ environmentVariableName: 'AUTH_TOKEN' });
+
+    const cacheClient = new SimpleCacheClient({
+      configuration: Configurations.Laptop.latest(),
+      credentialProvider: credentials,
+      defaultTtlSeconds: Number(process.env.CACHE_TTL)
+    });
+    momentoClient = cacheClient;
+
+    await initializeCaches(caches);
+  }
+
+  return momentoClient;
+};
+
+const getCacheAuthToken = async () => {
+  if (!cachedSecret) {
+    const command = new GetSecretValueCommand({ SecretId: process.env.MOMENTO_SECRET });
+    const result = await secretsManager.send(command);
+    cachedSecret = JSON.parse(result.SecretString).auth_token;
+  }
+  return cachedSecret;
+};
+
+const initializeCaches = async (caches) => {
+  if (caches?.length) {
+    const listCachesResponse = await momentoClient.listCaches();
+    const cachesToAdd = caches.filter(c => !listCachesResponse.caches.some(cache => cache.name == c));
+    for (const cacheToAdd of cachesToAdd) {
+      await momentoClient.createCache(cacheToAdd)
+    }
+  }
+}
